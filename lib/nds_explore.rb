dfb003734a37f99dc16a00eddac791f01d16b6a2
benign-fork-5056 <gareeeliu@gmail.com>
2020-03-03 02:32:59 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -7,11 +7,11 @@ def pretty_print_nds(nds)
   pp directors_database
 end
 
-def print_first_directors_movie_titles
-  index = 0
-  movies = directors_database[0][:movies]
-  while index < movies.count do
-    puts movies[index][:title]
-    index += 1
-  end
-end
+# def print_first_directors_movie_titles
+#   index = 0
+#   movies = directors_database[0][:movies]
+#   while index < movies.count do
+#     puts movies[index][:title]
+#     index += 1
+#   end
+# end
